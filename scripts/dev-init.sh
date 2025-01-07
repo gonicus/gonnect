3ea286581b7a58ae79c06f60b5416feb53939252
Cajus Pollmeier <pollmeier@gonicus.de>
2025-01-07 07:36:19 +0100

    chore: fix distrobox ini [skip ci]
    Adjust path for distrobox.

--- scripts/dev-init.sh
+++ scripts/dev-init.sh
@@ -11,6 +11,6 @@ cd $TMPDIR
 git clone https://github.com/pjsip/pjproject.git && cd pjproject
 git submodule update --init --recursive
 git checkout 2.15.1
-./configure --prefix=/usr/local --disable-video --disable-opus --enable-ext-sound CFLAGS="-fPIC -DPJ_HAS_IPV6=1"
+./configure --prefix=/opt/pjsip --disable-video --disable-opus --enable-ext-sound CFLAGS="-fPIC -DPJ_HAS_IPV6=1"
 make
 sudo make install
