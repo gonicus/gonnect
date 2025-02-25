811d3f1dafc03fdf0376e72bb88a61998401b2b2
Cajus Pollmeier <pollmeier@gonicus.de>
2025-02-25 14:08:37 +0100

    fix: avoid blocking mpris calls
    In combination with KDE connect, mpris DBus respose times
    go up to 30s - which leads to a near-blocking GOnnect UI.
    Work around these edge cases by setting a global timeout
    of 2s for mpris calls.

--- src/dbus/ExternalMediaManager.cpp
+++ src/dbus/ExternalMediaManager.cpp
@@ -4,6 +4,8 @@
 #include "MediaPlayerInterface.h"
 #include "ExternalMediaManager.h"
 
+#define MPRIS_CALL_TIMEOUT 2000
+
 Q_LOGGING_CATEGORY(lcExternalMedia, "gonnect.external.media")
 
 ExternalMediaManager::ExternalMediaManager(QObject *parent) : QObject(parent) { }
@@ -23,11 +25,16 @@ void ExternalMediaManager::pause()
             if (service.startsWith("org.mpris.MediaPlayer2.")) {
                 OrgMprisMediaPlayer2PlayerInterface mediaInterface(
                         service, "/org/mpris/MediaPlayer2", con, this);
+                mediaInterface.setTimeout(MPRIS_CALL_TIMEOUT);
 
                 if (mediaInterface.playbackStatus() == "Playing") {
                     qCDebug(lcExternalMedia) << "pausing" << service;
                     m_playerActiveTargets.append(service);
-                    mediaInterface.Pause();
+                    auto res = mediaInterface.Pause();
+                    if (res.isError()) {
+                        qCWarning(lcExternalMedia)
+                                << "failed to pause media player:" << res.error();
+                    }
                 }
             }
         }
@@ -44,9 +51,13 @@ void ExternalMediaManager::resume()
     for (auto &service : std::as_const(m_playerActiveTargets)) {
         OrgMprisMediaPlayer2PlayerInterface mediaInterface(service, "/org/mpris/MediaPlayer2", con,
                                                            this);
+        mediaInterface.setTimeout(MPRIS_CALL_TIMEOUT);
 
         qCDebug(lcExternalMedia) << "resuming" << service;
-        mediaInterface.Play();
+        auto res = mediaInterface.Play();
+        if (res.isError()) {
+            qCWarning(lcExternalMedia) << "failed to resume media player:" << res.error();
+        }
     }
 
     m_playerActiveTargets.clear();
@@ -61,7 +72,7 @@ QStringList ExternalMediaManager::getMprisTargets() const
                 QDBusMessage::createMethodCall("org.freedesktop.DBus", "/org/freedesktop/DBus",
                                                "org.freedesktop.DBus", "ListNames");
 
-        QDBusMessage reply = con.call(msg);
+        QDBusMessage reply = con.call(msg, QDBus::Block, MPRIS_CALL_TIMEOUT);
         if (!reply.arguments().isEmpty()) {
             return reply.arguments().at(0).toStringList();
         }
