1c72a69fa110693710fa15c869d2e4896f2f9d76
Markus Bader <bader@gonicus.de>
2025-02-27 15:12:34 +0100

    refactor: moved LDAP initialization to own file

--- src/contacts/LDAPAddressBookFeeder.h
+++ src/contacts/LDAPAddressBookFeeder.h
@@ -2,21 +2,15 @@
 
 #include <QObject>
 #include "IAddressBookFeeder.h"
+#include "LDAPInitializer.h"
 
 class LDAPAddressBookFeeder : public QObject, public IAddressBookFeeder
 {
     Q_OBJECT
 
 public:
-    enum class BindMethod { None, Simple, GSSAPI };
-    Q_ENUM(BindMethod)
-
-    explicit LDAPAddressBookFeeder(bool useSSL, const QString &ldapUrl, const QString &ldapBase,
-                                   const QString &ldapFilter, const BindMethod bindMethod,
-                                   const QString &bindDn, const QString &bindPassword,
-                                   const QString &saslRealm, const QString &saslAuthcid,
-                                   const QString &saslAuthzid, const QString &caFilePath,
-                                   QStringList sipStatusSubscriptableAttributes,
+    explicit LDAPAddressBookFeeder(const LDAPInitializer::Config &ldapConfig,
+                                   QStringList sipStatusSubscriptableAttributes = {},
                                    const QString &baseNumber = "", QObject *parent = nullptr);
 
     virtual void feedAddressBook(AddressBook &addressBook) override;
@@ -24,18 +18,7 @@ public:
 private:
     void clearCStringlist(char **attrs) const;
 
-    bool m_useSSL = true;
-    BindMethod m_bindMethod = BindMethod::None;
-    QString m_bindDn;
-    QString m_bindPassword;
-    QString m_ldapUrl;
-    QString m_ldapBase;
-    QString m_ldapFilter;
+    LDAPInitializer::Config m_ldapConfig;
     QString m_baseNumber;
-    QString m_saslRealm;
-    QString m_saslAuthcid;
-    QString m_saslAuthzid;
-    QString m_caFilePath;
-
     QStringList m_sipStatusSubscriptableAttributes;
 };
