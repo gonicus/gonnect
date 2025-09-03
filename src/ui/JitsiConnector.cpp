ba4ca9f8f8e256f8bdda072c8fa40b267a32f8fd
Markus Bader <mbgonicus@users.noreply.github.com>
2025-09-03 14:57:59 +0200

    fix: fix Jitsi Meet crash when upgrading SIP call with screen share (#162)
    * chore: better log output for Jitsi Meet JS errors
    
    * fix: fix Jitsi Meet crash when upgrading SIP call with screen share
    
    When upgrading a SIP call to a Jitsi Meet conference with the screen
    share button, it can happen that Jitsi Meet crashes when the
    toggleScreenShare command is invoked too early. A timer defers the call
    since no reliable event is available in the Jitsi Meet iframe API.

--- src/ui/JitsiConnector.cpp
+++ src/ui/JitsiConnector.cpp
@@ -96,7 +96,11 @@ void JitsiConnector::apiLoadingFinished()
 
     if (m_isToggleScreenSharePending) {
         m_isToggleScreenSharePending = false;
-        toggleScreenShare();
+
+        // It can sometimes crash the JS API, when the toggleScreenShare command is executed too
+        // early. Since there is no reliable event to listen to, a random timer must be used here to
+        // defer it such that Jitsi Meet is hopefully ready...
+        QTimer::singleShot(2000, this, [this]() { toggleScreenShare(); });
     }
 }
 
@@ -378,7 +382,7 @@ api.addListener("participantRoleChanged", data => {
 })
 
 api.addListener("errorOccurred", data => {
-    jitsiConn.addError(data.type, data.name, data.message, data.isFatal, data.details)
+    jitsiConn.addError(data.type, data.name, data.message, !!data.isFatal, data?.details ?? {})
 })
 
 api.addListener("incomingMessage", data => {
