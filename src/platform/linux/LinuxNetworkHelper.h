3e69ae5b36f3ee0f1b62bfed6be02b8437d26f0f
Cajus Pollmeier <pollmeier@gonicus.de>
2025-12-22 12:32:44 +0100

    chore: refactor network helper (#278)
    * Fix mac build
    * Refactor network helper

---
+++ src/platform/linux/LinuxNetworkHelper.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <QObject>
+#include "../NetworkHelper.h"
+
+class LinuxNetworkHelper : public NetworkHelper
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(LinuxNetworkHelper)
+
+public:
+    explicit LinuxNetworkHelper();
+
+    ~LinuxNetworkHelper() = default;
+};
