bb45139b1debf06ffc7ca689a8afea2097e1f7fb
LJ <150920490+ljgonicus@users.noreply.github.com>
2026-02-11 14:04:48 +0100

    fix: avoid local time conversion for unknown timezones (#333)

--- src/calendar/eds/EDSEventFeeder.cpp
+++ src/calendar/eds/EDSEventFeeder.cpp
@@ -128,11 +128,11 @@ QDateTime EDSEventFeeder::createDateTimeFromTimeType(ICalTime *datetime)
         return QDateTime();
     }
 
+    ICalTimezone *zone = i_cal_time_get_timezone(datetime);
     int daylight = i_cal_time_is_daylight(datetime);
-    int offset =
-            i_cal_timezone_get_utc_offset(i_cal_time_get_timezone(datetime), datetime, &daylight);
+    int offset = i_cal_timezone_get_utc_offset(zone, datetime, &daylight);
     QTimeZone convertZone(offset);
-    if (convertZone.isValid()) {
+    if (zone && convertZone.isValid()) {
         return QDateTime(QDate(i_cal_time_get_year(datetime), i_cal_time_get_month(datetime),
                                i_cal_time_get_day(datetime)),
                          QTime(i_cal_time_get_hour(datetime), i_cal_time_get_minute(datetime),
