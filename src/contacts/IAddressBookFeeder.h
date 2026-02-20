0f51ab30c89e1d17fe361830cd822febdc51d75f
Markus Bader <mbgonicus@users.noreply.github.com>
2026-02-20 10:09:06 +0100

    fix: do not call ldap feeder twice to prevent crash (#348)

--- src/contacts/IAddressBookFeeder.h
+++ src/contacts/IAddressBookFeeder.h
@@ -8,7 +8,10 @@ public:
     virtual void process() = 0;
     virtual QUrl networkCheckURL() const { return QUrl(); }
 
+    bool isProcessing() const { return m_isProcessing; }
+
 protected:
     QString m_displayName;
     unsigned m_priority = 0;
+    bool m_isProcessing = false;
 };
