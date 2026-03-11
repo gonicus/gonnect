1683c0ec12db75c9a872ec0c418f159a6cfcf271
Cajus Pollmeier <pollmeier@gonicus.de>
2026-03-11 16:40:12 +0100

    Show a different tray icon on registration problems

--- src/sip/SIPAccountManager.h
+++ src/sip/SIPAccountManager.h
@@ -43,6 +43,7 @@ Q_SIGNALS:
     void accountsChanged();
     void sipRegisteredChanged(bool status);
     void authorizationFailed(QString accountId);
+    void connectionError(int code, QString message);
 
 private:
     SIPAccountManager(QObject *parent = nullptr);
