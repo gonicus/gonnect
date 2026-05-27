f08f88e25b3bb4d3c4e3ce2165f7982029a6fc66
Cajus Pollmeier <pollmeier@gonicus.de>
2026-05-27 18:02:50 +0200

    chore: formatting

--- src/usb/busylight/BlinkStick.cpp
+++ src/usb/busylight/BlinkStick.cpp
@@ -25,22 +25,24 @@ void BlinkStick::send(bool on)
     const quint8 green = on ? static_cast<quint8>(m_color.green()) : 0x00;
     const quint8 blue = on ? static_cast<quint8>(m_color.blue()) : 0x00;
 
-    qCDebug(lcBlinkStick) << "Sending color to all" << ledCount << "LEDs: R=" << red << "G=" << green << "B=" << blue;
+    qCDebug(lcBlinkStick) << "Sending color to all" << ledCount << "LEDs: R=" << red
+                          << "G=" << green << "B=" << blue;
 
     bool success = true;
 
     for (int i = 0; i < ledCount; ++i) {
         unsigned char buf[6];
 
-        buf[0] = 0x05;      // Report ID: indexed color
-        buf[1] = 0x00;      // Channel
-        buf[2] = i;         // LED index
+        buf[0] = 0x05; // Report ID: indexed color
+        buf[1] = 0x00; // Channel
+        buf[2] = i; // LED index
         buf[3] = red;
         buf[4] = green;
         buf[5] = blue;
 
         if (hid_write(m_device, buf, sizeof(buf)) < 0) {
-            qCWarning(lcBlinkStick) << "failed to write to BlinkStick LED" << i << ":" << hid_error(m_device);
+            qCWarning(lcBlinkStick)
+                    << "failed to write to BlinkStick LED" << i << ":" << hid_error(m_device);
             success = false;
         }
     }
