09e7899b9ea869375d676fe78a2ba2b5af66cc15
Cajus Pollmeier <pollmeier@gonicus.de>
2026-06-27 09:17:38 +0200

    fix: potential crash in tray menu
    NumberStat may be a nullptr for certain contacts. Add a guard
    to avoid random crashes.

--- src/ui/SystemTrayMenu.cpp
+++ src/ui/SystemTrayMenu.cpp
@@ -27,7 +27,6 @@ SystemTrayMenu::SystemTrayMenu(QObject *parent) : QObject{ parent }
 
     m_trayIcon = new QSystemTrayIcon(this);
     m_trayIcon->setContextMenu(m_trayIconMenu);
-    m_trayIcon->show();
 
     connect(m_trayIcon, &QSystemTrayIcon::activated, this,
             [](QSystemTrayIcon::ActivationReason reason) {
@@ -81,6 +80,7 @@ SystemTrayMenu::SystemTrayMenu(QObject *parent) : QObject{ parent }
             &SystemTrayMenu::updateConferences);
 
     resetTrayIcon();
+    applyTrayIcon();
 }
 
 SystemTrayMenu::~SystemTrayMenu()
@@ -461,24 +461,33 @@ void SystemTrayMenu::updateBuddyState(const QString uri, SIPBuddyState::STATUS)
         QHashIterator favoriteIterator(m_favoriteActions);
         while (favoriteIterator.hasNext()) {
             favoriteIterator.next();
-            favoriteIterator.value()->setText(
-                    contactText(*(NumberStats::instance().numberStat(favoriteIterator.key()))));
+
+            if (const auto *stat = NumberStats::instance().numberStat(favoriteIterator.key())) {
+                favoriteIterator.value()->setText(contactText(*stat));
+            }
         }
 
         QHashIterator mostCalledIterator(m_mostCalledActions);
         while (mostCalledIterator.hasNext()) {
             mostCalledIterator.next();
-            mostCalledIterator.value()->setText(
-                    contactText(*(NumberStats::instance().numberStat(mostCalledIterator.key()))));
+
+            if (const auto *stat = NumberStats::instance().numberStat(mostCalledIterator.key())) {
+                mostCalledIterator.value()->setText(contactText(*stat));
+            }
         }
     } else {
         const auto number = PhoneNumberUtil::numberFromSipUrl(uri);
+        const auto *stat = NumberStats::instance().numberStat(number);
+        if (!stat) {
+            return;
+        }
 
         if (auto action = m_favoriteActions.value(number, nullptr)) {
-            action->setText(contactText(*(NumberStats::instance().numberStat(number))));
+            action->setText(contactText(*stat));
         }
+
         if (auto action = m_mostCalledActions.value(number, nullptr)) {
-            action->setText(contactText(*(NumberStats::instance().numberStat(number))));
+            action->setText(contactText(*stat));
         }
     }
 }
