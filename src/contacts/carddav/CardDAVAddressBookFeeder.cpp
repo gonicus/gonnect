d014428cb2e47460984e2fdb8491beabb7f43326
Markus Bader <bader@gonicus.de>
2026-07-20 13:18:08 +0200

    chore: debug output for CardDAV feeder detecting subscribable

--- src/contacts/carddav/CardDAVAddressBookFeeder.cpp
+++ src/contacts/carddav/CardDAVAddressBookFeeder.cpp
@@ -171,14 +171,28 @@ void CardDAVAddressBookFeeder::processVcard(QByteArray data, const QString &uuid
                 email = QString::fromStdString(prop.getValue());
             } else if (propName == "TEL") {
                 bool subscriptable = false;
+
+                qCDebug(lcCardDAVAddressBookFeeder)
+                        << "Checking card" << remoteUid << name << "if subscribable";
+                qCDebug(lcCardDAVAddressBookFeeder) << "Checking phone number" << prop.getValue();
+                qCDebug(lcCardDAVAddressBookFeeder)
+                        << "Subscribable types:" << m_sipStatusSubscriptableAttributes;
+
                 auto propParams = prop.params();
                 for (const auto &param : propParams) {
                     if (param.first == "TYPE") {
+
                         const auto types = QString::fromStdString(param.second)
                                                    .split(QChar(','), Qt::SkipEmptyParts);
+
+                        qCDebug(lcCardDAVAddressBookFeeder)
+                                << "Has types" << param.second << "parsed as" << types;
+
                         for (const auto &type : types) {
                             if (m_sipStatusSubscriptableAttributes.contains(
                                         type.trimmed().toLower())) {
+                                qCDebug(lcCardDAVAddressBookFeeder)
+                                        << "  subscribable type found:" << type;
                                 subscriptable = true;
                                 break;
                             }
@@ -436,6 +450,9 @@ void CardDAVAddressBookFeeder::process()
             settings.value("sipStatusSubscriptableAttributes", "").toStringList();
     m_sipStatusSubscriptableAttributes =
             subScriptableAttributes.isEmpty() ? QStringList() : subScriptableAttributes;
+
+    qCDebug(lcCardDAVAddressBookFeeder) << "Read subscribable attributes from config for" << m_group
+                                        << ":" << m_sipStatusSubscriptableAttributes;
 
     for (QString &attr : m_sipStatusSubscriptableAttributes) {
         attr = std::move(attr).toLower();
