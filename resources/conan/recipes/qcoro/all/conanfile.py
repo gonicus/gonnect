ac4ee845d11fad68fe83fd497e2f424dbebb4833
Cajus Pollmeier <pollmeier@gonicus.de>
2026-07-23 13:44:17 +0200

    Adjust qcoro conan setup

--- resources/conan/recipes/qcoro/all/conanfile.py
+++ resources/conan/recipes/qcoro/all/conanfile.py
@@ -1,6 +1,7 @@
+import os
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, rmdir
 
 
 class QCoro(ConanFile):
@@ -66,8 +67,42 @@ class QCoro(ConanFile):
         cmake = CMake(self)
         cmake.install()
 
+        cmake_base_dir = os.path.join(self.package_folder, 'lib', 'cmake', 'QCoro6Coro')
+
+        with open(os.path.join(cmake_base_dir, 'QCoroMacros.cmake'), 'r') as file:
+            data = file.read()
+
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+
+        os.makedirs(cmake_base_dir)
+        f = open(os.path.join(cmake_base_dir, 'QCoroMacros.cmake'), 'w')
+        f.write(data)
+        f.close()
+
     def package_info(self):
-        self.cpp_info.set_property("cmake_file_name", "qcoro")
-        self.cpp_info.set_property("cmake_target_name", "qcoro")
+        self.cpp_info.set_property("cmake_file_name", "QCoro6")
         self.cpp_info.set_property("cmake_find_mode", "both")
-        self.cpp_info.libs = ["qcorod" if self.settings.os == "Windows" and self.settings.build_type == "Debug" else "qcoro"]
+
+        self.cpp_info.set_property("cmake_build_modules", [
+            os.path.join("lib", "cmake", "QCoro6Coro", "QCoroMacros.cmake")
+        ])
+
+        self.cpp_info.components["core"].set_property("cmake_file_name", "QCoro6Core")
+        self.cpp_info.components["core"].set_property("cmake_target_name", "QCoro6::Core")
+        self.cpp_info.components["core"].libs = ["QCoro6Core"]
+        self.cpp_info.components["core"].includedirs = [os.path.join("include", "qcoro6"), os.path.join("include", "qcoro6", "qcoro")]
+
+        self.cpp_info.components["dbus"].set_property("cmake_file_name", "QCoro6DBus")
+        self.cpp_info.components["dbus"].set_property("cmake_target_name", "QCoro6::DBus")
+        self.cpp_info.components["dbus"].libs = ["QCoro6DBus"]
+        self.cpp_info.components["dbus"].requires = ["core"]
+
+        self.cpp_info.components["network"].set_property("cmake_file_name", "QCoro6Network")
+        self.cpp_info.components["network"].set_property("cmake_target_name", "QCoro6::Network")
+        self.cpp_info.components["network"].libs = ["QCoro6Network"]
+        self.cpp_info.components["network"].requires = ["core"]
+
+        self.cpp_info.components["websockets"].set_property("cmake_file_name", "QCoro6WebSockets")
+        self.cpp_info.components["websockets"].set_property("cmake_target_name", "QCoro6::WebSockets")
+        self.cpp_info.components["websockets"].libs = ["QCoro6WebSockets"]
+        self.cpp_info.components["websockets"].requires = ["core"]
