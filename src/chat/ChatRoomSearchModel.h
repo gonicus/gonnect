2ec81d30f56f7424e6cdd29d227aba501285c267
Markus Bader <mbgonicus@users.noreply.github.com>
2026-08-04 16:24:36 +0200

    refactor: use pagination on public chat room search results (#629)
    * refactor: use pagination on public chat room search results
    
    * chore: better default limit for chat room search
    
    * fix: fixed insert index in chat room search model
    
    * fix: do not process wrong search response
    
    * refactor: fixes for code review

--- src/chat/ChatRoomSearchModel.h
+++ src/chat/ChatRoomSearchModel.h
@@ -11,8 +11,12 @@ class ChatRoomSearchModel : public QAbstractListModel
     Q_OBJECT
     QML_ELEMENT
 
-    Q_PROPERTY(QString searchPhrase MEMBER m_searchPhrase NOTIFY searchPhraseChanged)
-    Q_PROPERTY(IChatProvider *chatProvider MEMBER m_chatProvider NOTIFY chatProviderChanged)
+    Q_PROPERTY(bool isLoading READ isLoading NOTIFY isLoadingChanged FINAL)
+    Q_PROPERTY(bool canLoadMore READ canLoadMore NOTIFY canLoadMoreChanged FINAL)
+    Q_PROPERTY(QString searchPhrase READ searchPhrase WRITE setSearchPhrase NOTIFY
+                       searchPhraseChanged FINAL)
+    Q_PROPERTY(quint32 limit MEMBER m_limit NOTIFY limitChanged FINAL)
+    Q_PROPERTY(IChatProvider *chatProvider MEMBER m_chatProvider NOTIFY chatProviderChanged FINAL)
 
 public:
     enum class Roles { Id = Qt::UserRole + 1, Name, Topic, NumberOfJoinedMembers, JoinRule };
@@ -23,17 +27,38 @@ public:
     virtual int rowCount(const QModelIndex &parent) const override;
     virtual QVariant data(const QModelIndex &index, int role) const override;
 
+    QString searchPhrase() const { return m_searchPhrase; }
+    void setSearchPhrase(const QString &phrase);
+
+    bool isLoading() const { return m_isLoading; }
+    bool canLoadMore() const { return m_canLoadMore; }
+
+    /// Load the next m_limit results
+    Q_INVOKABLE void loadNext();
+
 private Q_SLOTS:
     void onChatProviderChanged();
+    void updateModel();
 
 private:
+    void setIsLoading(bool value);
+    void setCanLoadMore(bool value);
+
+    bool m_isLoading = false;
+    bool m_isLoadingNext = false;
+    bool m_canLoadMore = false;
     QString m_searchPhrase;
-    QString m_lastSearchId;
+    QString m_searchTag;
+    QString m_nextBatchToken;
+    quint32 m_limit = 20;
     IChatProvider *m_chatProvider = nullptr;
     QObject *m_chatProviderContext = nullptr;
     QList<QSharedPointer<PublicChatRoom>> m_publicRooms;
 
 Q_SIGNALS:
+    void isLoadingChanged();
+    void canLoadMoreChanged();
     void chatProviderChanged();
     void searchPhraseChanged();
+    void limitChanged();
 };
