591475c85993f6824107869c00b840b4657496e8
Weblate (bot) <hosted@weblate.org>
2026-08-24 11:32:48 +0200

    chore(lang): translated using Weblate (German) (#665)
    Currently translated at 100.0% (1196 of 1196 strings)
    
    Translation: GOnnect/GOnnect
    Translate-URL: https://hosted.weblate.org/projects/gonnect/gonnect/de/
    
    Co-authored-by: Michael Neuendorf <neuendorf@gonicus.de>

--- i18n/gonnect_de.ts
+++ i18n/gonnect_de.ts
@@ -1326,7 +1326,7 @@
     <message>
         <location filename="../src/ui/components/popups/ChatUserSearch.qml" line="191"/>
         <source>No users found.</source>
-        <translation>Keine Benutzer gefunden</translation>
+        <translation>Keine Benutzer gefunden.</translation>
     </message>
     <message>
         <location filename="../src/ui/components/popups/ChatUserSearch.qml" line="193"/>
@@ -6346,8 +6346,8 @@
         <location filename="../src/ui/components/controls/VoiceMailField.qml" line="74"/>
         <source>%n old voice mail(s)</source>
         <translation>
-            <numerusform>%n neue Nachricht</numerusform>
-            <numerusform>%n neue Nachrichten</numerusform>
+            <numerusform>%n alte Nachricht</numerusform>
+            <numerusform>%n alte Nachrichten</numerusform>
         </translation>
     </message>
 </context>
